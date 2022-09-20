ac9d63a475cd80b52cea516354d16266686aa21a
Team Terraform CDK <github-team-tf-cdk@hashicorp.com>
2022-09-20 03:51:05 +0000

    chore: upgrade provider

--- src/multai-balancer.ts
+++ src/multai-balancer.ts
@@ -276,7 +276,7 @@ export class MultaiBalancer extends cdktf.TerraformResource {
       terraformResourceType: 'spotinst_multai_balancer',
       terraformGeneratorMetadata: {
         providerName: 'spotinst',
-        providerVersion: '1.83.0',
+        providerVersion: '1.84.0',
         providerVersionConstraint: '~> 1.0'
       },
       provider: config.provider,
