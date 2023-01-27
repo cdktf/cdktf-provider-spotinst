8998c45b70aa36c900436c54d35ab5505629401b
Team Terraform CDK <github-team-tf-cdk@hashicorp.com>
2023-01-27 03:02:47 +0000

    chore: upgrade provider from `1.96.0` to version `1.97.0`

--- src/ocean-spark/index.ts
+++ src/ocean-spark/index.ts
@@ -868,7 +868,7 @@ export class OceanSpark extends cdktf.TerraformResource {
       terraformResourceType: 'spotinst_ocean_spark',
       terraformGeneratorMetadata: {
         providerName: 'spotinst',
-        providerVersion: '1.96.0',
+        providerVersion: '1.97.0',
         providerVersionConstraint: '~> 1.0'
       },
       provider: config.provider,
