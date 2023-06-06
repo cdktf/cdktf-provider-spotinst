4ccf02fa69995a3a1fd5fed663a8e887faf9213f
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-06-06 11:48:42 +0000

    chore(deps): Updated

--- src/multai-target-set/index.ts
+++ src/multai-target-set/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/spotinst/spotinst/1.122.0/docs/resources/multai_target_set
 // generated from terraform resource schema
 
