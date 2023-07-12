6abdc45ad3464e9027d613fec3dbb2eb3e46f766
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-07-12 03:14:43 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/multai-balancer/index.ts
+++ src/multai-balancer/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/spotinst/spotinst/1.125.1/docs/resources/multai_balancer
 // generated from terraform resource schema
 
