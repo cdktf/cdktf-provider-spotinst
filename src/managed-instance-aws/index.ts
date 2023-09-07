782e7f86299323ba8503e62fb909c4133e878e68
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-09-07 03:07:56 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/managed-instance-aws/index.ts
+++ src/managed-instance-aws/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/spotinst/spotinst/1.135.0/docs/resources/managed_instance_aws
 // generated from terraform resource schema
 
