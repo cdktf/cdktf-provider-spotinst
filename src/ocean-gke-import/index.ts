4ea3b3c17ec7c80e50fbcdef5c4286a97e56870f
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-12-09 03:06:15 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/ocean-gke-import/index.ts
+++ src/ocean-gke-import/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/spotinst/spotinst/1.153.0/docs/resources/ocean_gke_import
 // generated from terraform resource schema
 
