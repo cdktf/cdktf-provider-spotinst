176aa80a3a342f5d3e0cb9becbdc9f294357171b
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-05-07 03:54:59 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/ocean-ecs-launch-spec/index.ts
+++ src/ocean-ecs-launch-spec/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/spotinst/spotinst/1.171.4/docs/resources/ocean_ecs_launch_spec
 // generated from terraform resource schema
 
