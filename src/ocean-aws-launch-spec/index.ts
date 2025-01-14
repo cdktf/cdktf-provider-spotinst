0737d5212208a0635666497ba6173f6ac5576df3
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-01-14 11:12:46 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@3d881cb

--- src/ocean-aws-launch-spec/index.ts
+++ src/ocean-aws-launch-spec/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/spotinst/spotinst/1.206.0/docs/resources/ocean_aws_launch_spec
 // generated from terraform resource schema
 
