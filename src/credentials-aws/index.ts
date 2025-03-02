def7884ddde3e0509d4777ae6884aeca3e577d92
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-03-02 06:18:18 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/credentials-aws/index.ts
+++ src/credentials-aws/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/spotinst/spotinst/1.210.0/docs/resources/credentials_aws
 // generated from terraform resource schema
 
