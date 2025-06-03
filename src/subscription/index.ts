975031f70fa02113014bc10f5f2c3867d9f4e55c
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-06-03 14:55:52 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@4d35a18

--- src/subscription/index.ts
+++ src/subscription/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/spotinst/spotinst/1.220.2/docs/resources/subscription
 // generated from terraform resource schema
 
